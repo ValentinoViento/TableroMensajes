b8a07c0de89152658fc5d221ddfca6617764ff24
ValentinoViento <vnbravo@udc.edu.ar>
2024-09-03 16:48:58 -0300

    Importado y sincronizado

---
+++ mensajes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from mensajes import views
+
+urlpatterns = [
+    path('', views.home_view, name="home"),
+    path('mensajes/', views.mensajes_view, name="mensajes"),
+    path('crear/', views.crear_view, name="crear"),
+    path('eliminar/', views.eliminar_view, name='eliminar'),
+    path('creación', views.MensajeView.as_view(), name='creación'),
+]
